4af215e5bc078c7dbe581afa5e3fccf6eece3b41
juliangibellino <jgibelli@172-30-28-106.dynapool.nyu.edu>
2014-04-05 12:55:17 -0400

    Initial commit
    Still need to fix delay. It is printing without the delay

--- countdown.js
+++ countdown.js
@@ -1,5 +1,11 @@
 function countdown(seconds){
-  // ...
-}
+	
+	for(seconds; seconds >= 0; seconds--){
+		var printTime = function(time){
+			console.log(time);
+		};
+		setTimeout(printTime(seconds), 1000);
+	}
+};
 
 countdown(10);

a022a3ef289fc468ef1cb6ee27052839734c44a7
Damian Kowal <damian@damiankowal.com>
2014-04-05 16:55:48 -0400

    Adding a space (no change in function)

--- countdown.js
+++ countdown.js
@@ -3,7 +3,7 @@ function countdown(seconds) {
     if (seconds > 0) {
         console.log(seconds);
         setTimeout(function() {
-            countdown(seconds -1);
+            countdown(seconds - 1);
         }, 1000);
     }
 }

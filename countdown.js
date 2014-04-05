2fe7d8f7975492e3df3bca2262f205936b5ed1d1
Damian Kowal <damian@damiankowal.com>
2014-04-05 12:35:07 -0400

    Version 1, using a global variable to keep track of time

--- countdown.js
+++ countdown.js
@@ -1,5 +1,10 @@
 function countdown(seconds){
-  // ...
+  // 1. Use global variable to keep track of time
+  time = seconds;
+  while (time > 0) {
+    console.log(time);
+    time -= 1;
+  }
 }
 
 countdown(10);

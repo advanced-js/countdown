84e34f28aea72be2d1f4c77919ecb75b8f8304c5
Damian Kowal <damian@damiankowal.com>
2014-04-05 16:08:48 -0400

    Using setInterval with global variables to count down time

--- countdown.js
+++ countdown.js
@@ -1,10 +1,18 @@
-function countdown(seconds){
-  // 1. Use global variable to keep track of time
-  time = seconds;
-  while (time > 0) {
-    console.log(time);
-    time -= 1;
-  }
+function countdown(seconds) {
+    // 1. Use global variable to keep track of time
+    time = seconds;
+    function decrementTime() {
+        time -= 1;
+        if (time > 0) {
+            console.log(time);
+        } else {
+            clearInterval(timer);
+        }
+    }
+    if (time > 0) {
+        console.log(time);
+        timer = window.setInterval(decrementTime, 1000);
+    }
 }
 
 countdown(10);

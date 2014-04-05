f27459532a4cdf38950c7f3ec5093b471c6ea212
Daniel Cohen <daniel.cohen@liveauctioneers.com>
2014-04-05 12:52:32 -0400

    levels 2 and 3

--- countdown.js
+++ countdown.js
@@ -1,5 +1,18 @@
-function countdown(seconds){
-  // ...
+function countdown_l2(seconds){
+  console.log(seconds);
+  setTimeout(function(){
+    if(seconds > 0){
+        countdown(seconds - 1);
+    }
+  }, 1000); 
 }
-
-countdown(10);
+function countdown_l3(seconds){
+    var intr = setInterval(function(){
+        console.log(seconds);
+        seconds--;
+        if(seconds < 0){
+            clearInterval(intr);
+        }
+    }, 1000);
+}
+countdown_l3(10);

2090e019134dfeaca06b87e681352a5398d42e77
Mark Polak <mark@172-30-28-125.dynapool.nyu.edu>
2014-04-05 12:55:04 -0400

    countdown to 0

--- countdown.js
+++ countdown.js
@@ -1,5 +1,14 @@
+
 function countdown(seconds){
-  // ...
+  document.write(seconds);
+  setInterval(function(){
+  		seconds = seconds-1;
+  		document.write(seconds + '<br>');
+	}, 1000);
+  setTimeout(function(){
+  	clearInteval()
+  	document.write(done);
+  }, seconds.getMilliseconds())
 }
 
 countdown(10);

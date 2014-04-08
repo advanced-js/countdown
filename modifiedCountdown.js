d3640ea548423e4c96e52cd5b5ba93caa1553a7b
Daniel Mulvihill <dmulvihill3@gmail.com>
2014-04-08 16:55:08 -0400

    Update modifiedCountdown.js

--- modifiedCountdown.js
+++ modifiedCountdown.js
@@ -1,6 +1,21 @@
-function countdown(x){
-	for (i=x;i>0;i--){
-	setTimeout("console.log(i)",5000);
-	}
-}
-countdown(10);
+<!doctype html>
+<html>
+	<meta charset="utf-8">
+	<title>Timer</title>
+	<body>
+		<div id="output"></div>
+		<script>
+		    function countdown(){
+			var output=document.getElementById('output');
+			output.innerHTML+=seconds+"<br>";
+			seconds--;
+			if (seconds<=0){
+				 output.innerHTML+="Blast Off!";
+				clearInterval(interval);
+			}
+        	     }
+		    var seconds=10;
+		    var interval=setInterval(countdown,1000);
+		</script>
+	</body>
+</html>

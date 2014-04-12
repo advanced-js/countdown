36d08c08f661fe57326f5db102ce3dae029ffca3
James Rothpearl <jrothpearl@gmail.com>
2014-04-11 22:17:07 -0400

    bonus solution with no new variables

--- countdown.js
+++ countdown.js
@@ -1,5 +1,17 @@
+//solution below doesn't use any new variables
+
 function countdown(seconds){
-  // ...
+
+  setInterval(function() {
+
+    if (seconds < 0) {
+      window.clearInterval();
+    } else {
+      console.log(seconds--);
+    };
+  }, 1000);
+
+
 }
 
-countdown(10);
+// countdown(10);

ceb721e9ddd6ec29e3fac5f6854c56af52ccd84f
Vu Nguyen <nnvu@outlook.com>
2014-05-31 12:54:19 -0400

    recursive countdown

--- countdown.js
+++ countdown.js
@@ -1,5 +1,10 @@
 function countdown(seconds){
-  // ...
+    if (seconds >= 0) {
+        console.log(seconds);
+        setTimeout(function(){
+            countdown(seconds-1)
+        }, 1000);
+    }
 }
 
 countdown(5);

d04b6b5031908b96121e1c87a65033e0a048c342
Zena Hirsch <hirsch.zena@gmail.com>
2014-05-31 16:25:26 -0400

    Create countdown function

--- countdown.js
+++ countdown.js
@@ -1,5 +1,12 @@
-function countdown(seconds){
-  // ...
+function countdown (seconds) {
+	var timer = setInterval(function() {
+		console.log(seconds);
+		if (seconds) {
+			seconds--;
+		} else {
+			clearInterval(timer);
+		}
+	}, 1000);
 }
 
 countdown(5);

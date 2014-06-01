666bf62f187b2a4a7d046fe10e9a6c3b21d12e15
msquarednet <msquarednet@gmail.com>
2014-06-01 14:30:26 -0400

    create countdown.js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,35 @@
+////////////////////////LEVEL 1
+/*
+var secs;
+
 function countdown(seconds){
-  // ...
+  secs = parseInt(seconds);
+  var clearIntervalID = setInterval(function() {
+  	console.log(secs--);
+	if (secs<0) {clearInterval(clearIntervalID);}
+  }, 1000);
 }
+*/
+
+///////////////////////LEVEL 2
+
+
+function countdown(seconds){
+  var secs = parseInt(seconds);
+  var clearIntervalID = setInterval(function() {
+  	console.log(secs--);
+	if (secs<0) {clearInterval(clearIntervalID);}
+  }, 1000);
+}
+
+
+///////////////////////LEVEL 3
+
+/*function countdown(seconds){
+	i give up 
+	:P
+}*/
+
+
 
 countdown(5);

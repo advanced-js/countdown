fea97e47c8058792fe29eb810d58a289e8a88169
Joe Burdick <josephdburdick@gmail.com>
2014-06-06 09:36:31 -0400

    Use global variable to keep track of time.

--- countdown.js
+++ countdown.js
@@ -1,5 +1,11 @@
+var timer;
 function countdown(seconds){
-  // ...
+  	timer = setInterval(function(){
+		console.log(seconds + '...');
+		seconds--;
+		if (seconds == -1)
+			clearInterval(timer);
+	}, 1000);
 }
 
 countdown(5);

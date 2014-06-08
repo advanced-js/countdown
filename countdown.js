c5323d45a2c4683d7b98875aed4c5b488d1f2a95
guddiya <vasisht.deepti@gmail.com>
2014-06-08 16:09:09 -0400

    Deepti's Level 1 Countdown submission
    Uses a global variable to keep track of time, while utilizing
    setInterval and clearInterval.

--- countdown.js
+++ countdown.js
@@ -1,5 +1,25 @@
-function countdown(seconds){
-  // ...
-}
+//Level 1: Uses global variable to keep track of time
 
-countdown(5);
+//Sets IntervalID
+var intervID = setInterval(countdown, 1000);
+
+//Declares and initializes Count variable
+var count = 0;
+
+//Declares and initializes seconds to countdown
+var seconds = 5;
+
+function countdown() {
+    // Prints the second countdown for as long as the count <= 5
+    if (count <= 5) 
+    {
+        console.log(seconds--);
+    }
+    else //Stops and clears the interval
+    {
+        clearInterval(intervID);
+    }
+
+    //Increments count
+    count++;
+}

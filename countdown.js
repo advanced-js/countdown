f95fc225f50f52e0a4a0114d230f7d94f27a1938
DJ Rosenbaum <dave@djrconcepts.com>
2014-09-26 00:10:02 -0400

    adding djrconcepts countdown Level2

--- countdown.js
+++ countdown.js
@@ -1,6 +1,7 @@
 /* ==== LEVEL 1 ==== */
 //Use global variable to keep track of time
 
+/*
 var seconds = 10;
 
 function countdown( seconds ) {
@@ -21,3 +22,24 @@ function countdown( seconds ) {
 }
 
 countdown(seconds);
+*/
+
+/* ==== LEVEL 2 ==== */
+//Keep track of time without defining any global variables
+
+function countdown( seconds ) {
+
+    if ( seconds >= 0 ) {
+
+        setTimeout(function () {
+
+            console.log( seconds );
+
+            countdown( seconds - 1 );
+
+        }, 1000);
+    }
+
+}
+
+countdown(10);

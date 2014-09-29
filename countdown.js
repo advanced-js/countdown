09cc1273e06f1813260f32dcd5005801280344c8
tsjoberg <tomgsjoberg@gmail.com>
2014-09-28 20:34:37 -0400

    countdown lvl 3

--- countdown.js
+++ countdown.js
@@ -1,5 +1,14 @@
 function countdown(seconds){
-  // ...
+  
+  if (seconds >=0){
+    
+    setTimeout(function(){
+      console.log(seconds);
+      countdown(seconds-1);
+    }, 1000);
+    
+  }
+
 }
 
 countdown(5);

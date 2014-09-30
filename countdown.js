2093854cb0883fa7dc76dbacaa383dc1994fa9b3
alexoviedo <alexoviedo999@gmail.com>
2014-09-30 00:30:55 -0400

    countdown level one.

--- countdown.js
+++ countdown.js
@@ -1,5 +1,19 @@
+// Countdown level 1.  Alejandro Oviedo
+
 function countdown(seconds){
-  // ...
+		
+	var myInterval = setInterval(function(){
+
+			seconds = seconds - 1;
+
+			if (seconds >= 0){
+				console.log(seconds);
+			}
+			else {
+				clearInterval(myInterval);
+			} 
+
+	}, 1000);
 }
 
 countdown(5);

39803bf2939a3e68971541a9a1cd01d6220ef446
Suzie Grange <suzie.g@me.com>
2014-09-30 22:15:46 -0400

    basic idiot-checking input type

--- countdown.js
+++ countdown.js
@@ -1,11 +1,15 @@
 function countdown(seconds) {
+  if (isNaN(seconds) && !parseInt(Number(seconds))) {
+    console.log('Please set a valid time');
+    return;
+  }
   if (seconds === 0) {
     console.log('BEEP BEEP BEEP');
-    return false;
+    return;
   }
   console.log(seconds);
   setTimeout(function () {
-    countdown(seconds -1);
+    countdown(seconds - 1);
   }, 1000);
 }
 

d34bba14f2c76bb55da2174169f43e7991564d83
Emily O'Brien <emily.obrien@refinery29.com>
2014-09-30 21:03:01 -0400

    countdown without new vars

--- countdown.js
+++ countdown.js
@@ -1,5 +1,13 @@
 function countdown(seconds){
-  // ...
-}
+
+	console.log(seconds);
+  	seconds -= 1;
+ 
+ 	if(seconds > 0){
+		setTimeout(function(){
+			countdown(seconds);
+  		}, 1000);
+  	}
+};
 
 countdown(5);

1e3694777073ac582afc99bb324e982278c25ce4
Suzie Grange <suzie.g@me.com>
2014-09-30 21:55:15 -0400

    countdown timer

--- countdown.js
+++ countdown.js
@@ -1,5 +1,12 @@
-function countdown(seconds){
-  // ...
+function countdown(seconds) {
+  if (seconds === 0) {
+    console.log('BEEP BEEP BEEP');
+    return false;
+  }
+  console.log(seconds);
+  setTimeout(function () {
+    countdown(seconds -1);
+  }, 1000);
 }
 
 countdown(5);

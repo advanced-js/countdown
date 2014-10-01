062fa734daf3f8c5eed1d9cd3ab21fa945e74d24
Jon Culver <jonathanhculver@gmail.com>
2014-09-30 21:34:12 -0400

    countdown functions

--- countdown.js
+++ countdown.js
@@ -1,5 +1,30 @@
-function countdown(seconds){
-  // ...
+function countdown(seconds) {
+	for(var delay = 1000; seconds>=0; seconds--, delay+=1000){
+		var count = function(seconds) {
+			setTimeout(function() {
+				console.log(seconds);
+			}, delay);
+		}
+		count(seconds);
+	}
 }
 
 countdown(5);
+
+
+
+function countdownRecursive(seconds) {
+	var count = function(seconds) {
+		setTimeout(function() {
+			console.log(seconds);
+			seconds--;
+			if(seconds >= 0) {
+				count(seconds);
+			}
+		}, 1000);
+	}
+
+	count(seconds);
+}
+
+//countdownRecursive(5);

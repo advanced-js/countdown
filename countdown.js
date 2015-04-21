7a98568880fe6966db019858ad90f42a192a3541
Caroline Steffen <csteffen@renttherunway.com>
2015-04-20 22:12:57 -0400

    creating kitchen timer

--- countdown.js
+++ countdown.js
@@ -1,5 +1,13 @@
 function countdown(seconds){
-  // ...
+	if (seconds === 0) {
+		return;
+	}
+
+	console.log(seconds);
+
+	setTimeout (function() {
+		countdown(seconds - 1)
+	}, 1000);
 }
 
 countdown(5);

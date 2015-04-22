e7ba2bebe2fbcf97b4813882116af4b0f18091e2
Runze <nyucps@ww311wk09.lab.scps.nyu.edu>
2015-04-22 13:53:26 -0400

    changed countdown.js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,22 @@
 function countdown(seconds){
   // ...
+  var intervalID;
+  function count() {
+  	console.log(seconds--);
+  	if(seconds<0)
+  		clearInterval(intervalID);
+  }
+  intervalID = setInterval(count, 1000);
+}
+
+//without creating new variables
+function countdown2(seconds) {
+		setTimeout(function(){
+			console.log(seconds--);
+			if(seconds>=0)
+				countdown2(seconds);
+		},1000);
 }
 
 countdown(5);
+countdown2(4);

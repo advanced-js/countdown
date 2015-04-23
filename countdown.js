38b762d9ab1f6ed80761da83b34df88f69ce22bd
Kobe Osei-Akoto <kobeosei-akoto@172-30-49-193.DYNAPOOL.NYU.EDU>
2015-04-23 18:40:19 -0400

    Countdown exercise

--- countdown.js
+++ countdown.js
@@ -1,5 +1,16 @@
-function countdown(seconds){
-  // ...
+var x = 10;
+var a = x;
+
+function countdown(x) {
+
+  if (a > -1) {
+     console.log(a); 
+    a--;
+  }
 }
 
-countdown(5);
+countdown(x);
+
+setInterval(function(){
+  countdown(x);
+}, 1000);

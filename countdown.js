55ec0797936556f0b568883ac847c0e1ca5a7401
August Hall <AugustHall@gmail.com>
2015-04-23 08:43:48 -0400

    ternary setInterval, trying to be fancy

--- countdown.js
+++ countdown.js
@@ -1,5 +1,8 @@
-function countdown(seconds){
-  // ...
+function countdown(seconds) {
+
+    setInterval(function() {
+        seconds >= 0 ? console.log(seconds--) : clearInterval();
+    }, 1000);
 }
 
-countdown(5);
+countdown(10);

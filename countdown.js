efbf3caa23582731a22425a657090224a41f72b6
August Hall <AugustHall@gmail.com>
2015-04-23 20:07:15 -0400

    fixed failed build for no value ternary

--- countdown.js
+++ countdown.js
@@ -1,7 +1,14 @@
 function countdown(seconds) {
 
     setInterval(function() {
-        seconds >= 0 ? console.log(seconds--) : clearInterval();
+        if (seconds >= 0) {
+            console.log(seconds);
+            seconds--;
+        } else {
+            clearInterval();
+        }
+        // no love ternary
+        // seconds >= 0 ? console.log(seconds--) : clearInterval();
     }, 1000);
 }
 

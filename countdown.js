b93889a17f40ce5cb7e2a84580f14fad43c9cf29
Juti Buican <jutiporn.buican@teamaol.com>
2015-10-10 14:26:36 -0400

    modify the code for display each second using console.log

--- countdown.js
+++ countdown.js
@@ -1,8 +1,20 @@
+//Exercise 2 - Countdown 
+function countdown(seconds) {
+  var timer = parseInt(seconds);
+  var delay = 1000;
+  try {
+    var startTimer = setInterval(function() {
+      console.log(timer);
+      timer--;
+      if (timer == 0) {
+        clearInterval(startTimer);
+      }
+    }, delay);
 
-onload = function() {
-  function countdown(seconds) {
-    // ...
-  }
+  } catch (e) {}
+}
+countdown(5);
 
-  countdown(5);
+function cancelSetInerval() {
+  clearInterval();
 }

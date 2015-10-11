e908e0580e8883e5cd4b412d87333ad0e3a83b88
mnsinger <mnsinger@gmail.com>
2015-10-11 10:56:50 -0400

    Works with global variables
    Do not understand why it works, but it seemingly does.

--- countdown.js
+++ countdown.js
@@ -1,5 +1,10 @@
+var t = -1;
+
 function countdown(seconds){
   // ...
+  window.setTimeout(function() { document.write(Math.abs(seconds - t) +'... <br>'); }, seconds * 1000);
+  t = t + 1;
+  if (seconds > 0) { countdown(seconds-1); }
 }
 
 countdown(5);

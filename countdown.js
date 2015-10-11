eee85aca13c3660883a49b6e2b05172956a93a30
mnsinger <mnsinger@gmail.com>
2015-10-11 19:46:37 -0400

    Added DOMContentLoaded Event
    Needed to wait for DOM to be loaded in order to write to the <body>.

--- countdown.js
+++ countdown.js
@@ -1,13 +1,15 @@
 function countdown(seconds){
   // ...
-  if (seconds > 0) {
+  if (seconds >= 0) {
     var i = window.setInterval(
       function() { 
-        document.write(seconds +'... <br>'); 
+        //document.write(seconds +'... <br>'); 
+        //console.log(seconds +'... <br>'); 
+        document.body.innerHTML += '<div>'+seconds+'...<div>';
         countdown(seconds-1);
-        window.clearInterval(i);
       }, 1000);
+    window.setTimeout(function () { window.clearInterval(i); }, 1000);
   }
 }
 
-countdown(5);
+document.addEventListener("DOMContentLoaded", countdown(5));

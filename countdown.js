c11740f4e292f1d59563fd7dca32dbf4604398ad
mnsinger <mnsinger@gmail.com>
2015-10-11 11:58:42 -0400

    Only works on Chrome
    Only works on chrome

--- countdown.js
+++ countdown.js
@@ -1,12 +1,13 @@
-// only works on chrome
-
-var t = -1;
-
 function countdown(seconds){
   // ...
-  window.setTimeout(function() { document.write(Math.abs(seconds - t) +'... <br>'); }, seconds * 1000);
-  t = t + 1;
-  if (seconds > 0) { countdown(seconds-1); }
+  if (seconds > 0) {
+    var i = window.setInterval(
+      function() { 
+        document.write(seconds +'... <br>'); 
+        countdown(seconds-1);
+        window.clearInterval(i);
+      }, 1000);
+  }
 }
 
 countdown(5);

104fd55950f0cb01e01911ddac73538197c81d5c
mariannedo <mariannepdo@gmail.com>
2015-10-12 09:12:30 -0400

    level 1?

--- countdown.js
+++ countdown.js
@@ -1,5 +1,14 @@
 function countdown(seconds){
-  // ...
+    var countdownBySeconds = setInterval(function() { 
+        if(seconds >= 0) { 
+            console.log(seconds); 
+            seconds--; 
+
+        } else { 
+            clearInterval(countdownBySeconds); 
+        }
+
+    }, 1000);
 }
 
 countdown(5);

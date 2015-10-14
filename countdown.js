553891614e33ccb5b64cb3b748f11013ed4aec5b
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-10-13 21:38:53 -0400

    complete countdown.js code

--- countdown.js
+++ countdown.js
@@ -1,5 +1,13 @@
 function countdown(seconds){
-  // ...
+  for (var i=0; i<=seconds; i++){
+    window.setTimeout(
+        function(n){
+          console.log(n+"...");
+        },
+        1000*i,
+        seconds-i
+    );
+  }
 }
 
 countdown(5);

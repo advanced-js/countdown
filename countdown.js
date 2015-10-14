73bf32e25ab9f56844e0bd8fddb6be778d64dd66
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-10-13 21:43:31 -0400

    don't create functions within loop

--- countdown.js
+++ countdown.js
@@ -1,12 +1,11 @@
+
+function log_seconds(n){
+  console.log(n+"...");
+}
+
 function countdown(seconds){
   for (var i=0; i<=seconds; i++){
-    window.setTimeout(
-        function(n){
-          console.log(n+"...");
-        },
-        1000*i,
-        seconds-i
-    );
+    window.setTimeout(log_seconds, 1000*i, seconds-i);
   }
 }
 

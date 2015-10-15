aea6b6bb62eb9b6fbfa119d98cf694a21254d068
Eric <ericblood1@gmail.com>
2015-10-15 16:50:05 -0400

    Add semicolon

--- countdown.js
+++ countdown.js
@@ -1,7 +1,7 @@
 function countdown(seconds){
 	if (seconds > -1) {
 		console.log(seconds);
-		setTimeout(function(){countdown(seconds - 1)},1000);
+		setTimeout(function(){countdown(seconds - 1);},1000);
 	}
 }
 

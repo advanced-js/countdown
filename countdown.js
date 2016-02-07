3475084dc1d6132b0498ae6a6f181c46f898320b
bluemittens505 <blaird2e@verizon.net>
2016-02-06 23:31:45 -0500

    Level 2
    Keep track of time without defining any global variables

--- countdown.js
+++ countdown.js
@@ -3,23 +3,42 @@
 
 // 3. BONUS: don't define any new variables
 
-function countdown(seconds) {
-	setTimeout(timer,1000,seconds);
-}
+// function countdown(seconds) {
+// 	setTimeout(timer,1000,seconds);
+// }
 
-function timer(seconds) {
-	if ( seconds > 0 ) {
-		document.write("<br/>" + seconds +  "...");
-	} else {
-		document.write("<br/>" + seconds);
-	}
-	seconds--;
-	if ( seconds >= 0 ) {
-		setTimeout(timer,1000,seconds);
-	}
-}
+// function timer(seconds) {
+// 	if ( seconds > 0 ) {
+// 		document.write("<br/>" + seconds +  "...");
+// 	} else {
+// 		document.write("<br/>" + seconds);
+// 	}
+// 	seconds--;
+// 	if ( seconds >= 0 ) {
+// 		setTimeout(timer,1000,seconds);
+// 	}
+// }
 
 // 2. Keep track of time without defining any global variables
+
+var intervalID;
+
+function countdown(seconds) {
+	var counter = {
+		total : seconds
+	};
+	intervalID = setInterval(timer,1000,counter);
+}
+
+function timer(counter) {
+	if ( counter.total === 0 ) {
+		document.write("<br/>" + counter.total);
+		clearInterval(intervalID);
+	} else {
+		document.write("<br/>" + counter.total + "...");
+		counter.total--;
+	}
+}
 
 // 1. Use global variable to keep track of time
 

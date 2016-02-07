669bf47a9a20f436b5454cdd80d1de93da84fa16
bluemittens505 <blaird2e@verizon.net>
2016-02-06 23:00:40 -0500

    Level 3
    BONUS: don't define any new variables
    Still need to do level 2

--- countdown.js
+++ countdown.js
@@ -1,22 +1,44 @@
 // Levels
-// 1. Use global variable to keep track of time
+// Note: All levels wait a second before starting the countdown
 
-var counter;
-var intervalID;
+// 3. BONUS: don't define any new variables
 
-function countdown(seconds){
-	counter = seconds;
-	intervalID = setInterval(timer,1000);
+function countdown(seconds) {
+	setTimeout(timer,1000,seconds);
 }
 
-function timer() {
-	if ( counter === 0 ) {
-		document.write("<br/>" + counter);
-		clearInterval(intervalID);
+function timer(seconds) {
+	if ( seconds > 0 ) {
+		document.write("<br/>" + seconds +  "...");
 	} else {
-		document.write("<br/>" + counter + "...");
-		counter--;
+		document.write("<br/>" + seconds);
+	}
+	seconds--;
+	if ( seconds >= 0 ) {
+		setTimeout(timer,1000,seconds);
 	}
 }
+
+// 2. Keep track of time without defining any global variables
+
+// 1. Use global variable to keep track of time
+
+// var counter;
+// var intervalID;
+
+// function countdown(seconds) {
+// 	counter = seconds;
+// 	intervalID = setInterval(timer,1000);
+// }
+
+// function timer() {
+// 	if ( counter === 0 ) {
+// 		document.write("<br/>" + counter);
+// 		clearInterval(intervalID);
+// 	} else {
+// 		document.write("<br/>" + counter + "...");
+// 		counter--;
+// 	}
+// }
 
 countdown(5);

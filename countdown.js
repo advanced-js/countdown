da96cf7c9497feba093c434ec81baaa1b55e7de3
bluemittens505 <blaird2e@verizon.net>
2016-02-06 22:19:15 -0500

    Level 1
    Use global variable to keep track of time

--- countdown.js
+++ countdown.js
@@ -1,5 +1,22 @@
+// Levels
+// 1. Use global variable to keep track of time
+
+var counter;
+var intervalID;
+
 function countdown(seconds){
-  // ...
+	counter = seconds;
+	intervalID = setInterval(timer,1000);
+}
+
+function timer() {
+	if ( counter === 0 ) {
+		document.write("<br/>" + counter);
+		clearInterval(intervalID);
+	} else {
+		document.write("<br/>" + counter + "...");
+		counter--;
+	}
 }
 
 countdown(5);
